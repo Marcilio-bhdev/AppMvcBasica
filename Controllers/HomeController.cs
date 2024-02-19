eb90ffe8e2eadbdaf29a4d19b78dda99e65e40b4
Marcilio dos Santos <mdsponto@gmail.com>
2024-02-19 19:29:00 -0300

    Mudanças Prova
    Ajustes necessários para prova teste da HI Plataform

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace AppMvcBasica.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult CalculaButton() 
+        {
+            return View();
+
+        }
     }
 }
---
+++ Models/PrecoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class PrecoModel
+    {
+        public double Custo { get; set; }
+        public double CustoAquisicao { get; set; }
+        public DateTime Validade { get; set; }
+        public DateTime ValidadeExpira { get; set; }
+        public bool Refrigeracao { get; set; }
+    }
+}
